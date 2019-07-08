4d85da2e11bfc1b3b927fca4e590e11ef8d78a8e
rafael lopez <rafael.lopez@movember.com>
2019-07-08 16:22:13 +1000

    lock data fields only

--- index.js
+++ index.js
@@ -2,52 +2,56 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 const connectedSockets = [];
+// This is mocking intial data coming from DB
+let serverData = { name: 'Rafael', lastName: 'Lopez', country: 'Spain' };
 
 // Sets a 3s timer, mocking a server call to save a file
-const mockSaveFile = async () => {
-	return new Promise(resolve => setTimeout(resolve, 3000))
-}
+const mockSaveFile = async (data) => {
+  serverData = { ...serverData, ...data};
+  return new Promise((resolve) => setTimeout(resolve, 3000));
+};
 
-const lockAllClients = () => {
-    connectedSockets.map((socket) => socket.emit('lock'));
-}
+const lockAllClients = (lockedData) => {
+  connectedSockets.map((socket) => socket.emit('lock', lockedData));
+};
 
 const unLockAllClients = () => {
-    connectedSockets.map((socket) => socket.emit('unlock'));
-}
+  connectedSockets.map((socket) => socket.emit('unlock'));
+};
 
-const broadcastNewData = (newData) => {
-    connectedSockets.map((socket) => socket.emit('newData', newData));
-}
+const broadcastNewData = () => {
+  connectedSockets.map((socket) => socket.emit('newData', serverData));
+};
 
 //Socket event config
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('new client connected');
   connectedSockets.push(socket);
+  broadcastNewData();
 
   socket.on('saveData', (data) => {
-      console.log('Data reveived form client', data);
-      console.log('Activating lock and sending data to S3');
-      // Lock all clients
-      lockAllClients();
-			// Save File
-			mockSaveFile().then(() => {
-				// Unlock all clients
-				unLockAllClients();
-				// Broadcast new data
-				broadcastNewData(data);
-			})
-  })
+    console.log('Data reveived form client', data);
+    console.log('Activating lock and sending data to S3');
+    // Lock all clients
+    lockAllClients(data);
+    // Save File
+    mockSaveFile(data).then(() => {
+      // Unlock all clients
+      unLockAllClients();
+      // Broadcast new data
+      broadcastNewData();
+    });
+  });
 
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 // Listen
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
